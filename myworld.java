428997dee25aaa7dd51ed88bd4a7daedbbc973da
Pratikshahosur <hosurpratiksha0102@gmail.com>
2025-04-22 08:28:35 +0530

    Update myworld.java

--- myworld.java
+++ myworld.java
@@ -2,7 +2,8 @@ public class myworld
 {
 public static void main(String [] agrs)
 {
-System.out.println("heyyyyy java")
+System.out.println("heyyyyy java");
+  System.out.println("byeeee java);
 }
 }
 
